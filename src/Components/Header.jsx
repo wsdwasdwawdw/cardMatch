a12a44fa2b3cc7fcdd2a5a2cd4dd8589d486b6a3
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-05-17 23:06:48 +0800

    Installed react framework

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+function Header() {
+
+    return(
+        <>
+            <h1>Get started</h1>   
+            <h2>Welcome to our app</h2>
+            <h3>I am Jason Karl Rosales</h3>
+
+        </>
+    )
+}
+
+export default Header
