92dbfc1541981a11549e1ba9b145a49595f049dc
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-05-19 23:12:51 +0800

    tried some things

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,12 +1,27 @@
-import Header from "./Header"
+
 function Container(){
+    const demo = (color) => {
+        if(color === "blue"){
+            alert("div is blue");
+        }
+        else{
+            alert("div is green");
+        }
+    }
+    
+    const shoot = () => {
+        alert("this is an alert");
+    }
+    
     return (
         <>
-        <div className="h-full flex flex-col justify-center align-center border">
-            <h1>Hello World</h1>
-        </div>
-    </>
-    )
+            <div className="p-2 flex justify-center align-center border">
+                <div onClick={() => demo("blue")} className="h-10 w-10 bg-blue-500"></div>
+                <div onClick={() => demo("green")} className="h-10 w-10 bg-green-500"></div>
+            </div>
+            <button onClick={shoot}>this is an alert</button>
+        </>
+    );
 }
 
 export default Container;
