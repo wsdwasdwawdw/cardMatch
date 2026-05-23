ccf46509fd68f510f9a733d746d34f049dcb1eb5
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-05-24 00:01:45 +0800

    feat: implement input validation and add Warning component

--- src/Components/Container.jsx
+++ src/Components/Container.jsx
@@ -1,23 +1,40 @@
 
 function Container(){
+
     const create = () => {
-        const input = document.querySelector('.input');
-        const value = input.value;
-        console.log(value);
-    }
-    
+        const input = document.querySelector('input');
+            let value = input.value;
+            if(value % 2 === 0){
+                alert("Even number detected!");
+                console.log("Even number found");
+                value = "";
+            }
+            else{
+                alert("Please enter an even number.");
+            }
+    };
+
+    const che = () => {
+        alert("this is a text field");
+    };
+
     return (
         <>
             <div className="flex gap-1">
-                <input type="number" className="input border px-1"/>
-                <button className="create border px-1">Create</button>
+                <input type="number" className="input border px-1" onClick={che}/>
+                <button className="create border px-1" onClick={create} title="Create">
+                    Create
+                </button>
             </div>
 
             <div>
                 
             </div>
         </>
+
+        
     );
+    
 }
 
 export default Container;
---
+++ src/Components/Warning.jsx
@@ -0,0 +1,11 @@
+function Warning(){
+    return (
+        <div className="warning">
+            
+            <h1>Warning!</h1>
+            <p>This is a warning message.</p>
+        </div>
+    );
+}
+
+export default Warning;
