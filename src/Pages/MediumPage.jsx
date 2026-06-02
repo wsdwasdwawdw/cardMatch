1dac6fd6a36ada2a0243726b28b8ef0bdbca5919
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-06-02 22:17:28 +0800

    added medium page

--- src/Pages/MediumPage.jsx
+++ src/Pages/MediumPage.jsx
@@ -1,6 +1,10 @@
 function MediumPage() {
     return(
         <>
+            <div>
+                <h1>Medium Page</h1>
+                <p>This is the medium difficulty page.</p>
+            </div>
         </>
     )
 }
