647785e0822ec9043910b1b4351f9165aed94309
wsdwasdwawdw <jasonkarlrosales@gmail.com>
2026-06-04 21:43:38 +0800

    added h1 elements

--- src/Pages/MediumPage.jsx
+++ src/Pages/MediumPage.jsx
@@ -4,6 +4,8 @@ function MediumPage() {
             <div>
                 <h1>Medium Page</h1>
                 <p>This is the medium difficulty page.</p>
+
+                <h1>I dont know how to do this properly</h1>
             </div>
         </>
     )
